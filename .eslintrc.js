ef556d56d5c528cff4d1b9bf18c8149d39c43207
Sal <codelur@gmail.com>
2025-01-14 18:35:24 -0800

    adding models and schemas

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+const _id = "m";
+
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+
+  // Add the necessary extensions.
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": ["error", { allow: ["warn"] }],
+    "no-underscore-dangle": "allow",
+  },
+};
---
+++ models/clothingItem.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: { type: String, required: true, minLength: 2, maxLength: 30 },
+  weather: { type: String, required: true, enum: ["hot", "warm", "cold"] },
+  imageUrl: {
+    type: String,
+    required: [true, "The imageUrl field is required."],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL.",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      default: [], // Default to an empty array
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("clothingItem", clothingItemSchema);
