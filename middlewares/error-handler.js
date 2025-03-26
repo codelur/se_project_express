30c97aaaa65f21a282685893c43a4f763aa27a71
Sal <codelur@gmail.com>
2025-03-25 21:12:54 -0700

    adding custom error classes

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,5 +1,14 @@
 
 const errorHandler = (err, req, res, next) => {
   console.error(err);
-  res.status(err.statusCode).send({ message: err.message });
+  // if an error has no status, set it to 500
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      // check the status and display a message based on it
+      message: statusCode === 500
+        ? 'An error occurred on the server'
+        : message
+    });
 }
