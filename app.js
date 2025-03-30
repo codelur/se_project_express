28339c703d9d70b90681ac47e9590cda2f1455d0
Sal <codelur@gmail.com>
2025-03-29 19:30:15 -0700

    adding crash test unit

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+
 app.post("/signin", login);
 app.post("/signup", createUser);
 
